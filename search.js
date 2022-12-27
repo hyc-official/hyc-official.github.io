fa25e8fdb22506fde1000df6715b8d8d0ba8fcae
Yangcheng He <191039275@qq.com>
2022-12-27 09:13:59 +0800

    Startpage: Add search engine, Moegirls
    And optimize JS code

--- search.js
+++ search.js
@@ -1,31 +1,11 @@
-baidu_url = "https://www.baidu.com/s?wd=\"%KeyWord%\"";
-bing_url = "https://cn.bing.com/search?q=%KeyWord%";
-fsou_url = "https://fsoufsou.com/search?q=%KeyWord%";
-search_url = baidu_url;
+urls = ["https://www.baidu.com/s?wd=\"%KeyWord%\"", "https://cn.bing.com/search?q=%KeyWord%", "https://fsoufsou.com/search?q=%KeyWord%", "https://zh.moegirl.org.cn/index.php?title=%KeyWord%"];
+names = ["百度", "必应", "F 搜", "萌娘百科"];
+search_url = urls[0];
 placeholder_template = "使用 %Word% 搜索";
-var baidu = function () {
-    search_url = baidu_url;
-    document.getElementById("search-box").placeholder = placeholder_template.replace(/%Word%/g, "百度");
-};
-var bing = function () {
-    search_url = bing_url;
-    document.getElementById("search-box").placeholder = placeholder_template.replace(/%Word%/g, "必应");
-};
-var fsou = function () {
-    search_url = fsou_url;
-    document.getElementById("search-box").placeholder = placeholder_template.replace(/%Word%/g, "F 搜");
-};
-var change = function () {
-    selection = document.getElementById("select-engine").selectedIndex;
-    if (selection == 0) {
-        baidu();
-    }
-    if (selection == 1) {
-        bing();
-    }
-    if (selection == 2) {
-        fsou();
-    }
+var searchengine_change = function () {
+    index = document.getElementById("select-engine").selectedIndex;
+    search_url = urls[index];
+    document.getElementById("search-box").placeholder = placeholder_template.replace(/%Word%/g, names[index]);
 };
 var redirect = function () {
     search_val = document.getElementById("search-box").value;
@@ -34,5 +14,24 @@ var redirect = function () {
 document.onkeydown = function () {
     if (event.keyCode == 13) {
         redirect();
+    }
+};
+
+var setcolor = function (color) {
+    document.getElementById("main").style.color = color;
+}
+Chtholly_times = 0;
+var searchtext_change = function () {
+    st = document.getElementById("search-box").value;
+    if (st == "珂朵莉" || st == "Chtholly") {
+        setcolor("#55c8fb");
+        document.getElementById("main-header").innerHTML = "Chtholly is so cute!";
+        if (++Chtholly_times >= 5) {
+            window.alert("珂朵莉是世界上最幸福最可爱的女孩子！\n出现此弹窗的原因：你已经输入了至少 5 次珂朵莉，这是起始页的彩蛋！");
+        }
+    }
+    else {
+        setcolor("#68e8bd");
+        document.getElementById("main-header").innerHTML = "Heyc's Startpage";
     }
 };
