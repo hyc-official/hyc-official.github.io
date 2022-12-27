fac991218a064568ee74ac0f0f078a2be8a000a8
Yangcheng He <191039275@qq.com>
2022-12-27 12:05:13 +0800

    Startpage: Add search engine, Baidu translate
    I can't be sure that Baidu translate is really a search engine :(

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
-urls = ["https://www.baidu.com/s?wd=\"%KeyWord%\"", "https://cn.bing.com/search?q=%KeyWord%", "https://fsoufsou.com/search?q=%KeyWord%", "https://zh.moegirl.org.cn/index.php?title=%KeyWord%"];
-names = ["百度", "必应", "F 搜", "萌娘百科"];
+urls = ["https://www.baidu.com/s?wd=\"%KeyWord%\"", "https://cn.bing.com/search?q=%KeyWord%", "https://fsoufsou.com/search?q=%KeyWord%", "https://zh.moegirl.org.cn/index.php?title=%KeyWord%", "https://fanyi.baidu.com/#auto/zh/%KeyWord%"];
+names = ["百度", "必应", "F 搜", "萌娘百科", "百度翻译"];
 search_url = urls[0];
 placeholder_template = "使用 %Word% 搜索";
 var searchengine_change = function () {
@@ -26,8 +26,11 @@ var searchtext_change = function () {
     if (st == "珂朵莉" || st == "Chtholly") {
         setcolor("#55c8fb");
         document.getElementById("main-header").innerHTML = "Chtholly is so cute!";
-        if (++Chtholly_times >= 5) {
-            window.alert("珂朵莉是世界上最幸福最可爱的女孩子！\n出现此弹窗的原因：你已经输入了至少 5 次珂朵莉，这是起始页的彩蛋！");
+        Chtholly_times++;
+        console.log("User input Chtholly for %d time(s)", Chtholly_times);
+        if (Chtholly_times >= 68) {
+            console.log("User trigger the bonus scene");
+            window.alert("珂朵莉是世界上最幸福最可爱的女孩子！\n注：你已经输入了至少 68 次珂朵莉！");
         }
     }
     else {
